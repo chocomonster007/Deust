df2080ded008124e7544ac6e2505bdec06604c03
unknown <antoinelecorre56@gmail.com>
2024-01-22 22:59:59 +0100

    debut ajout salle deust

--- src/script.js
+++ src/script.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 
 
@@ -9,6 +11,70 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+/**
+ * Loaders
+ */
+// Texture loader
+const textureLoader = new THREE.TextureLoader()
+
+// Draco loader
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('draco/')
+
+// GLTF loader
+const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
+
+const solBakedTexture = textureLoader.load('solEnd2.jpg')
+solBakedTexture.flipY = false
+solBakedTexture.colorSpace = THREE.SRGBColorSpace
+
+const plafondBakedTexture = textureLoader.load('plafond.jpg')
+plafondBakedTexture.flipY = false
+plafondBakedTexture.colorSpace = THREE.SRGBColorSpace
+
+console.log(plafondBakedTexture);
+
+const allBakedTexture = textureLoader.load('allFinal.jpg')
+allBakedTexture.flipY = false
+allBakedTexture.colorSpace = THREE.SRGBColorSpace
+
+const mobilierBakedTexture = textureLoader.load('ordis.jpg')
+mobilierBakedTexture.flipY = false
+mobilierBakedTexture.colorSpace = THREE.SRGBColorSpace
+
+const solBakedMaterial = new THREE.MeshBasicMaterial({
+    map:solBakedTexture
+})
+
+const plafondBakedMaterial = new THREE.MeshBasicMaterial({
+    map:plafondBakedTexture
+})
+
+const allBakedMaterial = new THREE.MeshBasicMaterial({
+    map:allBakedTexture
+})
+
+const mobilierBakedMaterial = new THREE.MeshBasicMaterial({
+    map:mobilierBakedTexture
+})
+
+
+gltfLoader.load('deustSalle.glb',gltf=>{
+    scene.add(gltf.scene)
+    const sol = gltf.scene.children.find(child=>child.name === 'sol')
+    console.log(gltf.scene.children);
+    const plafond = gltf.scene.children.find(child =>child.name === 'plafond')
+    const all = gltf.scene.children.filter(child=>['hautRetro','hautTable', 'tableBord','tableProf', 'controleRetro', 'loquetPorte','murFenetre', 'murFond', 'murPorte','murVideo', 'radiateur'].includes(child.name))
+    const mobilier = gltf.scene.children.filter(child=>['chaises','enceintes', 'myScreen','porteManteau','pancarteDeust','rétro','rétroprojecteur'].includes(child.name))
+    sol.material = solBakedMaterial
+    plafond.material = plafondBakedMaterial
+    all.forEach(child=>{
+        child.material = allBakedMaterial
+    })
+    mobilier.forEach(child=>child.material = mobilierBakedMaterial)
+    //all.material = allBakedMaterial
+})
 
 // Sizes
 const sizes = {
