bedaf33383719c11f5b94a35ae1e907a75f80d5a
unknown <antoinelecorre56@gmail.com>
2024-02-03 10:59:10 +0100

    liitleChnage

--- src/shader/ecranVertex.glsl
+++ src/shader/ecranVertex.glsl
@@ -1,6 +1,7 @@
-
+varying float vPosition;
 void main(){
     vec4 modelPosition = modelMatrix * vec4(position,1.0);
+    vPosition = abs(cos(modelPosition.x))
     vec4 viewPositon = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPositon;
 
