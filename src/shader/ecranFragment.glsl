93b5de686bbebbaafd48ab12cac4983d83134821
unknown <antoinelecorre56@gmail.com>
2024-02-08 00:47:21 +0100

    pouf

--- src/shader/ecranFragment.glsl
+++ src/shader/ecranFragment.glsl
@@ -1,5 +1,8 @@
-varying float vPosition;
+varying float vPositionX;
+varying float vPositionY;
+uniform float uTime;
 void main(){
-    float color = vPosition;
+    float timeTruc = 1.01-uTime/10.0;
+    float color = step(timeTruc,1.0-abs(vPositionX*vPositionY));
     gl_FragColor = vec4(color,color,color,1.0);
 }
