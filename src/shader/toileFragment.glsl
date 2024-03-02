a942e8c9d71cdb3681661920583d07e9031df9f9
unknown <antoinelecorre56@gmail.com>
2024-03-02 23:26:00 +0100

    ahaha

--- src/shader/toileFragment.glsl
+++ src/shader/toileFragment.glsl
@@ -1,4 +1,11 @@
+uniform sampler2D uTexture;
+
+varying vec2 vUv;
+varying float vPos;
 
 void main(){
-    gl_FragColor = vec4(1.0,1.0,1.0,1.0);
+
+    vec4 textureColor = texture2D(uTexture, vUv);
+    vec4 colorF = vec4(textureColor.x+vPos,textureColor.y+vPos,textureColor.z+vPos,1.0);
+    gl_FragColor = colorF;
 }
